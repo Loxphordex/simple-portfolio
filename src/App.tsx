aba4941d64e5f781f40b111fa4356e354d113bbe
Silas <silasishallahan@gmail.com>
2021-02-24 09:09:17 -0800

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header/Header'
 import ProjectList from './Project/ProjectList'
+import Footer from './Footer/Footer'
 import './scheme.scss'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Header />
       <ProjectList />
+      <Footer />
     </div>
   )
 }
