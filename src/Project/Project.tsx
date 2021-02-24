ce259be03eaa10bc76b45e021db3511a4078488f
Silas <silasishallahan@gmail.com>
2021-02-24 13:35:49 -0800

    project links shadow

--- src/Project/Project.tsx
+++ src/Project/Project.tsx
@@ -52,10 +52,18 @@ export default function Project({name, theme, links, info, desktopImages, mobile
           {renderMobileImages()}
         </div>
       </div>
-      <div className='project-links'>
-        <a href={links.liveSite} target='blank'>Live Site</a>
-        <a href={links.githubClient} target='blank'>Github Client</a>
-        {links.githubServer && <a href={links.githubServer} target='blank'>Github Server</a>}
+      <div className='project-links '>
+        <div className='link-border'>
+          <a className='project-link drop-shadow' href={links.liveSite} target='blank'>Live Site</a>
+        </div>
+        <div className='link-border'>
+          <a className='project-link drop-shadow' href={links.githubClient} target='blank'>Github Client</a>
+        </div>
+        {links.githubServer &&
+          <div className='link-border'>
+            <a className='project-link drop-shadow' href={links.githubServer} target='blank'>Github Server</a>
+          </div>
+        }
       </div>
     </section>
   )
