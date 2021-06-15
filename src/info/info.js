76be1e859ffba904e8f8bb23056039bfd36b79da
Silas <silasishallahan@gmail.com>
2021-06-15 15:00:52 -0700

    new pear text

--- src/info/info.js
+++ src/info/info.js
@@ -1,7 +1,7 @@
 const pearegrine = [
-  'Pearegrine is a clothing company based in New York. I created this website using React and implemented transaction functionality via Stripe API.',
-  'The creator of the Pearegrine brand has access to an admin UI that allows him to add, delete, and modify all aspects of the items for sale. This is true for the “Discover” blog as well, which features the Quill JS text editor, accessible only to the admin. This blog will be updated soon.',
-  'The brand is still young and has not been supplied yet. However, full purchasing capabilities are implemented and ready to use at any moment. At this moment, if a user attempts to make a purchase, they will be directed to Stripe’s test checkout, and no real money will be exchanged.'
+  'Pearegrine is an independent clothing company based in New York. I created its website using React, Express, and the Postgres database, and implemented transaction functionality via Stripe API. Users can purchase items and store a cart, as well as visit the “Discover” blog section.',
+  'The creator of the Pearegrine brand has access to an admin UI that allows him to add, delete, and modify all aspects of the items for sale. This is true for the “Discover” blog as well, which features a Quill JS text editor, accessible only to the admin.',
+  'The brand is still young and has not been supplied yet. The designs you see are placeholders, used to demonstrate what the brand could be in the future. However, full purchasing capabilities are implemented and ready to use at any moment. In the site’s current state, if a user attempts to make a purchase they will be directed to Stripe’s test checkout, and no real money will be exchanged.'
 ]
 
 const memoryField = [
